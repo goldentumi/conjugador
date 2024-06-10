8c55283f2d3844da92c69be46b1c3a9daf79c0de
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-06-10 17:28:27 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -59,10 +59,11 @@ dict_que_esp = dict(zip(quechua,espanol))
 base = st.selectbox(
     
     "Seleccione un verbo en quechua",quechua)
+st.write("El verbo en español es:", dict_que_esp[base])
 if base.endswith("y"):
    base = base[:-1]
 
-#st.write("El verbo en español es:", dict_que_esp[base])
+
 
 
 #para persona
