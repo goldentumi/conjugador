ff09ff15880082890c3b7c6a6d46000161d955bb
Gabriela Vásquez Tumi <157500489+goldentumi@users.noreply.github.com>
2024-06-10 16:46:03 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -5,6 +5,50 @@
 import pandas as pd
 
 verbos = pd.read_excel ('quechua.xlsx')
+# -*- coding: utf-8 -*-
+
+
+#Tablas de conjugación en excel
+import pandas as pd
+quechua = pd.read_excel('quechua.xlsx')
+
+#Abrir todas las hojas del excel
+excel_file = pd.ExcelFile('quechua.xlsx')
+D = {}
+for hoja in excel_file.sheet_names:
+  df = pd.read_excel('quechua.xlsx', sheet_name = hoja)
+  c = df.columns
+  df.set_index(c[0], inplace = True)
+  print(f'Hoja: {hoja}')
+  print(df.head())
+
+  d = df.to_dict()
+
+  D[hoja] = d
+
+#Diccionario
+
+
+pronombre = pd.read_excel('quechua.xlsx', sheet_name = 'pronombres')
+col = pronombre.columns
+pronombre.set_index(col[0], inplace=True)
+d_pronombre = pronombre.to_dict()
+
+#Diccionario de pronombres
+
+
+#Función para conjugar verbos
+def conjugador(base,persona,numero,tiempo):
+  return d_pronombre[numero][persona] + ' ' + base + D[tiempo][numero][persona]
+
+conjugador('miku','segunda','plural','presentesimple')
+
+#Para ingresar más fácilmente las condiciones
+base = input('Ingrese base aquí:')
+persona= input('Ingrese persona (diferencia primera incl / excl):')
+numero = input('Ingrese número (singular / plural):')
+tiempo = input('Ingrese tiempo (sinespacios):')
+
 
 ##diccionario
 
